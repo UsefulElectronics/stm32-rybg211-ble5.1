8c4e2c397cfadfb666a23d34053881a643f72c97
Ward Almasarani <wardzx@hotmail.com>
2022-12-19 22:50:14 +0300

    Best PWM duty value is 10% for the power LEDs

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -216,7 +216,7 @@ static void MX_TIM3_Init(void)
     Error_Handler();
   }
   sConfigOC.OCMode = TIM_OCMODE_PWM1;
-  sConfigOC.Pulse = 0;
+  sConfigOC.Pulse = 10;
   sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
   sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
   if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
